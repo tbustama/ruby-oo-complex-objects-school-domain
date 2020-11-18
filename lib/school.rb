1bf735fadbc8b4d8c722897c7f973248e2d5b2d1
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-18 12:08:46 -0800

    Sorted some hashes

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+    attr_accessor :name
+    attr_reader :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    
+
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        hash = {}
+        arr = @roster.sort
+        arr.each do |ele|
+            hash[ele[0]] = ele[1].sort
+        end
+        hash
+    end
+
+end
